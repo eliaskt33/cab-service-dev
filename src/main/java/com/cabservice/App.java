404accf64d79dde4c14967bd5646bd0a255ccde5
Elias <egty3328@gmail.com>
2026-01-21 20:10:10 +0200

    Fix BOM encoding issue in Java files

---
+++ src/main/java/com/cabservice/App.java
@@ -0,0 +1,11 @@
+package com.cabservice;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Cab Service - Maven project is running!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
