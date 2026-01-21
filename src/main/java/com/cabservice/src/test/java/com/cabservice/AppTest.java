ca08a055a5e3787b2e06eab4762ca0a05fb16255
eliaskt33 <egty3328@gmail.com>
2026-01-21 18:58:50 +0200

    Add unit test for sample functionality

---
+++ src/main/java/com/cabservice/src/test/java/com/cabservice/AppTest.java
@@ -0,0 +1,11 @@
+package com.cabservice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
